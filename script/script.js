350c6f5d332de4b10a8f3cdb13f37a7fa2968705
Satria <satriadwi.2410u@gmail.com>
2026-04-24 07:37:44 +0700

    update

--- script/script.js
+++ script/script.js
@@ -147,7 +147,7 @@ async function prosesBayar() {
     const currentUser = JSON.parse(localStorage.getItem("user"));
 
     if (!currentUser) {
-        window.location.href = "/";
+        window.location.href = "/Anoms-Transc-KasirMieAyam";
     }
 
     const bayarInput = document.getElementById("bayar");
